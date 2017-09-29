8ce4c15243aa0ddfaf62cea83d58673e590d2fc9
Mike Thomas <mathomas@users.noreply.github.com>
2017-09-29 16:39:25 -0600

    Add docker installation.  Increase memory (Java eats memory like peanuts).

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,15 @@
 # -*- mode: ruby -*-
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
-
   config.vm.network "private_network", ip: "192.168.33.10"
-
   config.vm.provision "shell", 
                       path: "provision.sh",
                       env: {
-                        }
+                      }
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 3072
+    v.cpus = 2 
+  end
 end
 
--- provision.sh
+++ provision.sh
@@ -34,8 +34,28 @@ function git() {
   apt-get install -y git
 }
 
+function docker() {
+  apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+  add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+  apt-get update
+  apt-get install -y --force-yes docker-ce
+
+  #usermod -a -G docker $USER
+}
+
 once prep
 once update
 once jdk
 once jenkins
 once git
+once docker
